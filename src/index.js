f341849fa50e8897ce6e356aaa1483075f91eb81
MLCampbell <marcus.l.campbell@gmail.com>
2021-09-23 20:49:07 +1200

    Change index.js and App.js for mapping components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
 
 ReactDOM.render(
-    <h1>Test</h1>,
-  document.getElementById('root')
+  <div>
+    <h1>My Contacts</h1>
+    <Card
+      class="my-style"
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="+123 456 789"
+      mail="b@beyonce.com"
+    />
+    <Card
+      name="Jack Bauer"
+      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      tel="+987 654 321"
+      mail="jack@nowhere.com"
+    />
+    <Card
+      name="Chuck Norris"
+      img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+      tel="+918 372 574"
+      mail="gmail@chucknorris.com"
+    />
+  </div>,
+  document.getElementById("root")
 );
+
+
+
+{/* <h2></h2>
+<img
+  src=""
+  alt="avatar_img"
+/>
+<p></p>
+<p></p>
+
+<h2></h2>
+<img
+  src=""
+  alt="avatar_img"
+/>
+<p></p>
+<p></p> */}
