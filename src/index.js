02f9388281d79883223d87f61950250e0e23dc97
MLCampbell <marcus.l.campbell@gmail.com>
2021-09-27 19:12:18 +1300

    Refactor with arrow functions

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />,document.getElementById("root")
+ReactDOM.render(<App />, document.getElementById("root")
 );
+
+// var numbers = [3, 56, 2, 48, 5];
+
+// const double = numbers.map(num => num * 2)
+// console.log(double);
+
+// const greaterTen = numbers.filter(num => num > 10);
+// console.log(greaterTen);
+
+// const total = numbers.reduce((accum, num) => accum + num);
+// console.log(total);
+
+// const findForty = numbers.find(num => num >= 40);
+// console.log(findForty);
+
+// const findIndexFifty = numbers.findIndex(num => num >= 50);
+// console.log(findIndexFifty);
