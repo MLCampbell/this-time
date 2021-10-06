c06b56f11f2ee11359a1301baefd9b9f7a6ec038
MLCampbell <marcus.l.campbell@gmail.com>
2021-10-06 19:47:36 +1300

    Handling events using state & conditonal rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,36 @@ import React, { useState } from "react";
 
 function App() {
 
-  const [count, setCount] = useState(0);
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMousedOver, setMouseOver] = useState(false)
 
-  function increase() {
-    setCount(count + 1);
+
+  function handleClick() {
+    setHeadingText("Submitted");
   };
 
-  function decrease() {
-    setCount(count - 1);
+  function handleMouseOver() {
+    setMouseOver(true);
   };
 
-  return <div className="container">
-    <h1>{count}</h1>
-    <button onClick={increase}>+</button>
-    <button onClick={decrease}>-</button>
-  </div>
-};
+  function handleMouseOut() {
+    setMouseOver(false);
+  };
+
+  return (
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button 
+      style={{backgroundColor: isMousedOver ? "black" : "white"}}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+      onClick={handleClick}
+      >
+      Submit
+      </button>
+    </div>
+  );
+}
 
 export default App;
